c05b0984f97117e2864eae87ad57f8d0ea494596
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2024-08-20 17:00:21 +0530

    complete project

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { shadesOfPurple } from "@clerk/themes";
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+console.log(PUBLISHABLE_KEY);
+const KEY = import.meta.env.VITE_SUPABASE_URL;
+console.log("key",KEY);
+
+
+
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ClerkProvider
+      appearance={{
+        baseTheme: shadesOfPurple,
+      }}
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl="/"
+    >
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
